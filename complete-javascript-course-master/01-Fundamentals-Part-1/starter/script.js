eccc9171ae20fa0d9dd88e3949b145a4abb903c9
idc1407 <idc1407@gmail.com>
2023-08-15 18:58:27 +0530

    s202308151858

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -56,7 +56,7 @@ const birthYear = 1991;
 
 var job = 'programmer';
 job = 'teacher';
-*/
+
 
 const now = 2023;
 const ageJonas = now - 2001;
@@ -70,6 +70,116 @@ const firstName = 'Jonas';
 const lastName = 'Mann';
 console.log(firstName +' '+ lastName);
 
+// Assignment Operators
 let x = 10 + 5; // 15
-x += 10; // x = x + 10
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
 console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2023;
+const ageJonas = now - 2001;
+const ageSarah = now - 2006;
+
+console.log(now - 2001 > now - 2006);
+
+let x,y;
+x = y = 25 -10 -5; // x = y = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+console.log(massMark);
+console.log(heightMark);
+console.log(massJohn);
+console.log(heightJohn);
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+
+const BMIMark1 = massMark / (heightMark * heightMark);
+const BMIJohn1 = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, BMIMark1, BMIJohn1, markHigherBMI);
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ',' + " a " + (year - birthYear) + " years old " + job + '!';
+
+const jonasNew = `I'm ${firstName}, a ${year-birthYear} years old ${job}!`
+
+console.log(jonas);
+console.log(jonasNew);
+
+console.log(`I'm a cool person`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`)
+*/
+
+const age = 19;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license')
+}else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years `)
+}
+
+const birthYear = 1998;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+// Go through vid.18 last part once again
