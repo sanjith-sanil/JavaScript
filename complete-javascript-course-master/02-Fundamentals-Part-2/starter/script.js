376e811a3b4da10c81beb5007e5b555d07ce73fc
idc1407 <idc1407@gmail.com>
2023-08-23 22:53:59 +0530

    s202308232253

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,5 @@
 // DAY 5:
+
 /*
 function logger() {
     console.log('My name is Sanjith');
@@ -28,6 +29,14 @@ function calcAge1(birthYear) {
 const age1 = calcAge1(1991);
 console.log(age1);
 
+// Function declaration(alternate)
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear;
+    return age;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
 // Function expression
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
@@ -37,18 +46,66 @@ const age2 = calcAge2(1991);
 console.log(age1, age2);
 
 
-//Arrow function
+// Arrow function
 const calcAge3 = birthYear => 2023 - birthYear;
 const age3 = calcAge3(2001);
 console.log(age3);
 
 const yearsUntilRetirement = (birthYear,firstName) => {
     const age = 2023 - birthYear;
-    const retirement = 65- age;
+    const retirement = 65 - age;
     //return retirement;
     return `${firstName} retiresin ${retirement} years`;
 }
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1991, 'Bob'));
+
+
+// DAY 6: Re-doing D5
+
+// DAY 7:
+
+// function cutPieces(fruit) {
+//     return fruit * 4;
+// }
+
+const cutPieces = function(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangesPieces = cutPieces(oranges);
+    
+    return `Juice with ${applePieces} A.Pieces & ${orangesPieces} Or.Pieces ${apples} apples and ${oranges} oranges.`;
+}
+
+console.log(fruitProcessor(2, 3));
+// const juice = fruitProcessor(2, 3);
+// console.log(juice);
+
+
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+Your tasks:
+
+Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+
+Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+
+Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+
+Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+
+Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
+
+TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
 */
