aa519ffff2f57c877e08eb65d1f1624083411ade
idc1407 <idc1407@gmail.com>
2023-08-28 22:56:20 +0530

    s202308282256

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -80,7 +80,7 @@ console.log(fruitProcessor(2, 3));
 // console.log(juice);
 
 
-CHALLENGE #1
+//CHALLENGE #1
 Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
 
 Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
@@ -133,8 +133,9 @@ console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
 */
 
-//ARRAYS
 /*
+//ARRAYS
+
 const friend1 = 'Micheal';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -175,7 +176,6 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
 console.log(ages);
-*/
 
 const friends = ['Micheal', 'Steven', 'Peter'];
 console.log(friends);
@@ -195,10 +195,88 @@ friends.pop(); //Last (Jay)
 //friends.pop(); //Last (Peter)
 
 const popped = friends.pop();
-console.log(popped); //shows which one deleted
+console.log(popped); //showed which one was deleted
 console.log(friends);
 
 friends.shift(); //deleting at the start
 console.log(friends);
 
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); // no element called Bob (so -1)
 
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob')); // no element called Bob (so false)
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
+
+//CHALLENGE #2
+
+const calcTip = function (bill) {
+    return bill >=50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
+
+
+//OBJECTS
+
+const jonas = {
+    firstName: 'Sanjith',
+    lastName: 'Sanil',
+    age: 2023 - 2001,
+    job: 'teacher',
+    friends: ['Micheal', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]); //first + Name = firstName(Sanjith)
+console.log(jonas['last' + nameKey]);
+
+//console.log(jonas.'last' + nameKey) //doesn't work
+
+const interestedIn =  prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+console.log(jonas[interestedIn]);
+
+if(jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');    
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@sanjithsanil';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Micheal"
+console.log(`${jonas.firstName} has ${jonas.friends.length}, and his best friend is called ${jonas.friends[0]}`)
+*/
+
+const jonas = {
+    firstName: 'Sanjith',
+    lastName: 'Sanil',
+    birthYear: 2001,
+    job: 'teacher',
+    friends: ['Micheal', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function(birthYear) {
+        return 2023 - birthYear;
+    }
+};
+
+console.log(jonas.calcAge(2001));
+console.log(jonas['calcAge'](2001));
