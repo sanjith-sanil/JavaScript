b465e48d8e7da4588db91959fbddb3d4554548df
sanjith-sanil <138775818+sanjith-sanil@users.noreply.github.com>
2023-08-28 12:52:59 +0530

    s202308281252

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -140,6 +140,7 @@ checkWinner(scoreDolphins, scoreKoalas);
 */
 
 //ARRAYS
+/*
 const friend1 = 'Micheal';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -147,16 +148,66 @@ const friend3 = 'Peter';
 const friends = ['Micheal', 'Steven', 'Peter'];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
+const y = new Array(1991, 1984, 2008, 2020);
 
 console.log(friends[0]);
 console.log(friends[2]);
 
 console.log(friends.length);
-console.log(friends[friends.length - 1]) // 3(0,1,2) elements -> 3-1 -> 2nd one displayed
+
+console.log(friends.length - 1);
+console.log(friends[friends.length - 1]);
 
 friends[2] = 'Jay';
+//Array is primitive, so even const value can be changed(but not whole array)
 console.log(friends);
+
+const firstName = 'Jonas'
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
+console.log(ages);
+*/
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+// Adding
+//friends.push('Jay'); adding element in end
+const newLength = friends.push('Jay');
+console.log(friends);
+
+console.log(newLength);
+
+friends.unshift('John'); //adding element in beginning
+console.log(friends);
+
+// Removing
+friends.pop(); //Last (Jay)
+//friends.pop(); //Last (Peter)
+
+const popped = friends.pop();
+console.log(popped); //shows which one deleted
+console.log(friends);
+
+friends.shift(); //deleting at the start
+console.log(friends);
+
+
 
 
 
