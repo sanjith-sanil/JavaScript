f51bdc5ea1599af9fc123570f837edede83b727a
idc1407 <idc1407@gmail.com>
2023-09-06 23:02:17 +0530

    s202309062302

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -69,10 +69,14 @@ const measureKelvin = function () {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
-    value: prompt('Degrees celsius:'),
+
+    // C) FIX
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
   };
 
-  console.log(measurement);
+  // B) FIND
+  console.table(measurement);
 
   // console.log(measurement.value);
   // console.warn(measurement.value);
@@ -83,3 +87,25 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 0, 5]);
+// A)
+console.log(amplitudeBug);
