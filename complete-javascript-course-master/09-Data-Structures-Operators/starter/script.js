831bead8765f6322a2b6cd7fca63088ad35ef1a7
idc1407 <idc1407@gmail.com>
2023-10-09 22:57:10 +0530

    s202310092257

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -53,7 +53,35 @@ const restaurant = {
   },
 };
 
-// restaurant.numGuests = 0;
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
 
@@ -61,7 +89,6 @@ console.log(guests);
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
 
-/*
 // Short Circuiting (&& and ||)
 
 console.log('---- OR ----');
